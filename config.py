022be7ebbac1e018cc9e2192f463d5909bd34471
alexdobleboom <santanamedinaalexdenis590@gmail.com>
2024-09-26 14:45:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 # Credenciales de la API de Telegram
 API_ID = 22788599                    # Reemplaza con tu api_id
 API_HASH = "6fd904cf42bbe1f6d57f22d8d392e9b4"                # Reemplaza con tu api_hash
-BOT_TOKEN = "8189951868:AAFzHLcEa12GFtkPUKuNcIVV6EIDOGo9_vs"              # Reemplaza con tu bot_token
+BOT_TOKEN = "7636488480:AAFXEhpgFhh6Hs1N4H-8zEEe4-h7OyGTsr4"              # Reemplaza con tu bot_token
 MY_USER_ID = 7459926358                 # Reemplaza con tu user_id
 USERNAME = "@Music_botsupreme"                 # Reemplaza con tu username de Telegram
