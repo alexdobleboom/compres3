8373d6b083f6d75ec2d4eb46d5206c7d84c7f29b
꧁༺🆇🅳_🆂🅻🅰🆈🅴🆁༻꧂ <santanamedinaalexdenis590@gmail.com>
2024-10-05 03:22:04 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # Credenciales de la API de Telegram
-API_ID = 22788599                    # Reemplaza con tu api_id
-API_HASH = "6fd904cf42bbe1f6d57f22d8d392e9b4"                # Reemplaza con tu api_hash
-BOT_TOKEN = "7636488480:AAFXEhpgFhh6Hs1N4H-8zEEe4-h7OyGTsr4"              # Reemplaza con tu bot_token
-MY_USER_ID = 7459926358                 # Reemplaza con tu user_id
-USERNAME = "@Music_botsupreme"                 # Reemplaza con tu username de Telegram
+API_ID = 24288670                    # Reemplaza con tu api_id
+API_HASH = ""                # Reemplaza con tu api_hash
+BOT_TOKEN = ""              # Reemplaza con tu bot_token
+MY_USER_ID = 7803998694                 # Reemplaza con tu user_id
+USERNAME = "animalbotproyect"                 # Reemplaza con tu username de Telegram
