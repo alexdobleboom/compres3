c3cd13da64e8b43a709f8afd8b668f04d7f4d893
꧁༺🆇🅳_🆂🅻🅰🆈🅴🆁༻꧂ <santanamedinaalexdenis590@gmail.com>
2024-10-05 03:27:21 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # Credenciales de la API de Telegram
 API_ID = 24288670                    # Reemplaza con tu api_id
-API_HASH = ""                # Reemplaza con tu api_hash
+API_HASH = "81c58005802498656d6b689dae1edacc"                # Reemplaza con tu api_hash
 BOT_TOKEN = "7779702228:AAGZ_rauBecTu4PT1gzvBKWXWJY3oXlKTIY"              # Reemplaza con tu bot_token
 MY_USER_ID = 7803998694                 # Reemplaza con tu user_id
 USERNAME = "animalbotproyect"                 # Reemplaza con tu username de Telegram
