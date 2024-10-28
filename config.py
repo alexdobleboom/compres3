92086ad887e45f4ac537e9bd6ff2c028b435cce4
𖣘︎𝘿𝙚𝙨𝙩𝙧𝙤𝙮֍ <santanamedinaalexdenis590@gmail.com>
2024-10-27 20:25:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@
 API_ID = 24288670                    # Reemplaza con tu api_id
 API_HASH = "81c58005802498656d6b689dae1edacc"                # Reemplaza con tu api_hash
 BOT_TOKEN = "7779702228:AAGZ_rauBecTu4PT1gzvBKWXWJY3oXlKTIY"              # Reemplaza con tu bot_token
-MY_USER_ID = 7803998694                 # Reemplaza con tu user_id
-USERNAME = "animalbotproyect"                 # Reemplaza con tu username de Telegram
+MY_USER_ID = 7551486576               # Reemplaza con tu user_id
+USERNAME = "TheDemonsuprem"                 # Reemplaza con tu username de Telegram
