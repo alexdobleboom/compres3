00fcf12b8dfed56aa2b403ac571be75c009b6519
𖣘︎𝘿𝙚𝙨𝙩𝙧𝙤𝙮֍ <santanamedinaalexdenis590@gmail.com>
2024-10-27 20:23:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,9 +73,7 @@ def help_command(client, message: Message):
     app.send_message(chat_id=message.chat.id, text="Comandos disponibles:\n"
                                                     "/start - Inicia el bot.\n"
                                                     "/help - Muestra esta ayuda.\n"
-                                                    "/compress - Comprime videos.\n"
-                                                    "/descompress - Descomprime archivos en formato .zip.\n"
-                                                    "/picarzip - Divide archivos en partes.\n"
+                                                    "/convert - Comprime videos.\n"
                                                     "/add - Añade usuarios autorizados.\n"
                                                     "/ban - Banea a un usuario del bot.\n"
                                                     "/addadmin - Da permisos de administración a un usuario.\n"
@@ -88,7 +86,7 @@ def help_command(client, message: Message):
                                                     "/listagrup - Lista de grupos autorizados.\n"
                                                     "/status - Muestra el estatus de un usuario.")
 
-@app.on_message(filters.command("compress"))
+@app.on_message(filters.command("convert"))
 async def compress_video(client, message: Message):  # Cambiar a async
     username = message.from_user.username or f"user_{message.from_user.id}"
 
@@ -137,7 +135,7 @@ async def compress_video(client, message: Message):  # Cambiar a async
 
             # Descripción para el video comprimido
             description = (
-                f"꧁༺ 𝑷𝒓𝒐𝒄𝒆𝒔𝒐 𝑭𝒊𝒏𝒂𝒍𝒊𝒔𝒂𝒅𝒐 ༻꧂\n"
+                f"꧁𝑷𝒓𝒐𝒄𝒆𝒔𝒐 𝑭𝒊𝒏𝒂𝒍𝒊𝒔𝒂𝒅𝒐꧂\n"
                 f"⏬ 𝑷𝒆𝒔𝒐 𝑶𝒓𝒊𝒈𝒊𝒏𝒂𝒍: {original_size // (1024 * 1024)} MB\n"
                 f"⏫ 𝑷𝒆𝒔𝒐 𝑷𝒓𝒐𝒄𝒆𝒔𝒂𝒅𝒐: {compressed_size // (1024 * 1024)} MB\n"
                 f"▶️ 𝑻𝒊𝒆𝒎𝒑𝒐 𝒅𝒆 𝑷𝒓𝒐𝒄𝒆𝒔𝒂𝒎𝒊𝒆𝒏𝒕𝒐: {processing_time_str}\n"
@@ -157,82 +155,6 @@ async def compress_video(client, message: Message):  # Cambiar a async
                 os.remove(compressed_video_path)
     else:
         await app.send_message(chat_id=message.chat.id, text="‼️𝑹𝒆𝒔𝒑𝒐𝒏𝒅𝒆 𝒂 𝒖𝒏 𝒗𝒊𝒅𝒆𝒐 𝒑𝒂𝒓𝒂 𝒄𝒐𝒎𝒑𝒓𝒊𝒎𝒊𝒓𝒍𝒐‼️.")
-
-@app.on_message(filters.command("descompress"))
-async def decompress_file(client, message: Message):
-    username = message.from_user.username or f"user_{message.from_user.id}"
-
-    if not is_user_authorized(username):
-        #await app.send_message(chat_id=message.chat.id, text="❌𝑵𝒐 𝒑𝒐𝒔𝒆𝒆 𝒂𝒄𝒄𝒆𝒔𝒐❌.")
-        return
-
-    if message.reply_to_message and message.reply_to_message.document:
-        archive_path = await app.download_media(message.reply_to_message.document)
-        file_extension = os.path.splitext(archive_path)[1].lower()
-        extract_folder = "extracted_files"
-
-        if file_extension != '.zip':
-            await app.send_message(chat_id=message.chat.id, text="𝑬𝒍 𝑭𝒐𝒓𝒎𝒂𝒕𝒐 𝒅𝒆𝒍 𝒂𝒓𝒄𝒉𝒊𝒗𝒐 𝒅𝒆𝒗𝒆 𝒅𝒆 𝒔𝒆𝒓 👉.zip.")
-            return
-
-        os.makedirs(extract_folder, exist_ok=True)
-        await app.send_message(chat_id=message.chat.id, text="↗️𝑬𝒏 𝑷𝒓𝒐𝒈𝒓𝒆𝒔𝒐...↘️")
-
-        try:
-            with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-                zip_ref.extractall(extract_folder)
-
-            await app.send_message(chat_id=message.chat.id, text="😁𝑭𝒊𝒏𝒂𝒍𝒊𝒛𝒂𝒅𝒐😁.")
-            for file in os.listdir(extract_folder):
-                await app.send_document(chat_id=message.chat.id, document=os.path.join(extract_folder, file))
-
-        except Exception as e:
-            await app.send_message(chat_id=message.chat.id, text=f"⭕𝑬𝒓𝒓𝒐𝒓 𝒂𝒍 𝒅𝒆𝒔𝒄𝒐𝒎𝒑𝒓𝒊𝒎𝒊𝒓 𝒆𝒍 𝒂𝒓𝒄𝒉𝒊𝒗𝒐⭕: {e}")
-        finally:
-            if os.path.exists(archive_path):
-                os.remove(archive_path)
-
-            shutil.rmtree(extract_folder)  # Elimina el folder de extracción
-    else:
-        await app.send_message(chat_id=message.chat.id, text="‼️𝑹𝒆𝒔𝒑𝒐𝒏𝒅𝒂 𝒂 𝒖𝒏 𝒂𝒓𝒄𝒉𝒊𝒗𝒐.zip ‼️.")
-
-@app.on_message(filters.command("picarzip"))
-async def split_file(client, message: Message):
-    username = message.from_user.username or f"user_{message.from_user.id}"
-
-    if not is_user_authorized(username):
-        #await app.send_message(chat_id=message.chat.id, text="❌𝑵𝒐 𝒑𝒐𝒔𝒆𝒆 𝒂𝒄𝒄𝒆𝒔𝒐❌.")
-        return
-
-    if message.reply_to_message and message.reply_to_message.document:
-        file_path = await app.download_media(message.reply_to_message.document)
-        parts_list = []
-        part_sizes = [5 * 1024 * 1024, 15 * 1024 * 1024, 25 * 1024 * 1024, 50 * 1024 * 1024, 100 * 1024 * 1024]  # Tamaños en bytes
-
-        await app.send_message(chat_id=message.chat.id, text="↗️𝑷𝒓𝒐𝒄𝒆𝒔𝒐 𝒆𝒏 𝒎𝒂𝒓𝒄𝒉𝒂...↘️")
-        file_size = os.path.getsize(file_path)
-        part_num = 1
-
-        while file_size > 0:
-            size = min(part_sizes[-1], file_size)  # Usar el tamaño máximo definido
-            part_filename = f"{file_path}.part{part_num}"
-
-            with open(part_filename, 'wb') as part_file:
-                with open(file_path, 'rb') as original_file:
-                    part_file.write(original_file.read(size))
-
-            parts_list.append(part_filename)
-            file_size -= size
-            part_num += 1
-
-        for part in parts_list:
-            await app.send_document(chat_id=message.chat.id, document=part)
-            os.remove(part)
-
-        await app.send_message(chat_id=message.chat.id, text="𝑪𝒐𝒎𝒑𝒍𝒆𝒕𝒂𝒅𝒐👌.")
-        os.remove(file_path)  # Eliminar el archivo original después de dividir
-    else:
-        await app.send_message(chat_id=message.chat.id, text="‼️𝑹𝒆𝒔𝒑𝒐𝒏𝒅𝒆 𝒂 𝒖𝒏 𝒂𝒓𝒄𝒉𝒊𝒗𝒐 𝒑𝒂𝒓𝒂 𝒅𝒊𝒗𝒊𝒅𝒊𝒓𝒍𝒐‼️.")
 
 @app.on_message(filters.command("add"))
 def add_user(client, message: Message):
@@ -343,7 +265,7 @@ def list_admins(client, message: Message):
     admins = "\n".join(admin_users)
     app.send_message(chat_id=message.chat.id, text=f"𝑨𝒅𝒎𝒊𝒏 📔:\n{admins}")
 
-@app.on_message(filters.command("listagrup"))
+@app.on_message(filters.command("listgrup"))
 def list_groups(client, message: Message):
     app.send_message(chat_id=message.chat.id, text=f"𝑮𝒓𝒖𝒑𝒐𝒔 👥:\n{', '.join(groups)}")
 
@@ -357,7 +279,7 @@ def user_status(client, message: Message):
     else:
         app.send_message(chat_id=message.chat.id, text="Por favor proporciona un @ de usuario.")
 
-@app.on_message(filters.command("acceso"))
+@app.on_message(filters.command("open"))
 def acceso_command(client, message: Message):
     username = message.from_user.username or f"user_{message.from_user.id}"
     add_authorized_user(username)  # Asegura que se agregue al usuario
